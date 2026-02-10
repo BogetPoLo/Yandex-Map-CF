b107f538f3e4ed551cb6d50de1c1a3f4c143b025
designer <da@ya.ru>
2026-02-10 16:13:00 +0300

    create search

---
+++ search.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import requests
+import arcade
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "MAP"
+MAP_FILE = "map.png"
+
+
+class GameView(arcade.Window):
+    def setup(self):
+        self.get_image()
+
+    def on_draw(self):
+        self.clear()
+
+        arcade.draw_texture_rect(
+            self.background,
+            arcade.LBWH(
+                (self.width - self.background.width) // 2,
+                (self.height - self.background.height) // 2,
+                self.background.width,
+                self.background.height
+            ),
+        )
+
+    def get_ll(self, geocode):
+        server_address = 'http://geocode-maps.yandex.ru/1.x/?'
+        api_key = '8013b162-6b42-4997-9691-77b7074026e0'
+        geocode = geocode
+
+        geocoder_request = f'{server_address}apikey={api_key}&geocode={geocode}&format=json'
+
+        response = requests.get(geocoder_request)
+        if response:
+            # Преобразуем ответ в json-объект
+            json_response = response.json()
+
+            # Получаем первый топоним из ответа геокодера.
+            # Согласно описанию ответа, он находится по следующему пути:
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            # Полный адрес топонима:
+            toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+            # Координаты центра топонима:
+            toponym_coodrinates = toponym["Point"]["pos"]
+            # Печатаем извлечённые из ответа поля:
+            print(toponym_address, "имеет координаты:", toponym_coodrinates)
+            return ','.join(toponym_coodrinates.split(" "))
+        else:
+            print("Ошибка выполнения запроса:")
+            print(geocoder_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+
+    def get_image(self):
+        server_address = 'https://static-maps.yandex.ru/v1?'
+        api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+        arena = ['Москва, Спартак Арена', 'ВТБ Арена Динамо', 'Москва, Большая спортивная арена Лужники']
+        cor = [(self.get_ll(ar)) for ar in arena]
+        # Готовим запрос.
+        map_request = f"https://static-maps.yandex.ru/v1?lang=ru_RU&pt={cor[0]},pm2rdl~{cor[1]},pm2bll~{cor[2]},pm2grl&apikey={api_key}"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+            # Запишем полученное изображение в файл.
+        with open(MAP_FILE, "wb") as file:
+            file.write(response.content)
+
+        self.background = arcade.load_texture(MAP_FILE)
+
+
+def main():
+    gameview = GameView(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+    gameview.setup()
+    arcade.run()
+    # Удаляем за собой файл с изображением.
+    os.remove(MAP_FILE)
+
+
+if __name__ == "__main__":
+    main()
